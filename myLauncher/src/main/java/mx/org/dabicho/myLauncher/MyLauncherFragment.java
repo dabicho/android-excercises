c8fb630b65eca03df95de5c6208db275dfa5b139
dabicho <tsukebumi@gmail.com>
2014-10-14 07:33:47 -0500

    launcher

---
+++ myLauncher/src/main/java/mx/org/dabicho/myLauncher/MyLauncherFragment.java
@@ -0,0 +1,11 @@
+package mx.org.dabicho.myLauncher;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+
+/**
+ * Created by dabicho on 13/10/14.
+ */
+public class MyLauncherFragment extends ListFragment {
+
+}
---
+++ myLauncher/src/main/java/mx/org/dabicho/myLauncher/SingleSupportFragmentActivity.java
@@ -0,0 +1,49 @@
+package mx.org.dabicho.myLauncher;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Clase que extiende de Activity y que inicializa para contener un fragmento
+ */
+abstract public class SingleSupportFragmentActivity extends FragmentActivity{
+    private static final String TAG="SingleFragmentActivity";
+
+
+
+    /**
+     *
+     * @return El fragmento que hospeda esta actividad
+     */
+    protected abstract Fragment createFragment();
+
+    /**
+     * Método para ser sobreescrito por clases herederas para devolver el id del resource que va
+     * a ser expuesto en la interfaz
+     * @return
+     */
+    protected int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(getLayoutResId());
+
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        Fragment lFragment=fm.findFragmentById(R.id.fragmentContainer);
+
+        if(lFragment==null){
+            lFragment=createFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer,lFragment).commit();
+        }
+
+
+    }
+}
